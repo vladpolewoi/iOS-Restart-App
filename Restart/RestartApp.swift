339bceb894f138776d85d5530dbf2658d83edb59
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-11-25 22:11:09 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Quest76 on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
